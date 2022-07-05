073664aabe1a4ad60cf9b087e63b77b483f0cd8f
Ivan Kopiev <kopiev@mustdev.ru>
2022-07-05 08:13:38 +0300

    Update targets

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Eazyshow",
+    platforms: [
+        SupportedPlatform.iOS(.v14),
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +21,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Eazyshow",
-            dependencies: []),
-        .testTarget(
-            name: "EazyshowTests",
-            dependencies: ["Eazyshow"]),
+        .binaryTarget(name: "Eazyshow", url: "https://mustdev.ru/Eazyshow.xcframework.zip", checksum: "8387713b8dbc1a2083e0548c3ee15621f068effc86e8217870e72f441aef7aca")
     ]
 )
